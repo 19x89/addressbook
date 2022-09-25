a2d7fbbc1e603f71a1c8c063b30456a7e1c900ed
19x89 <33625931+19x89@users.noreply.github.com>
2022-09-25 12:50:50 +0200

    Update FullPersistence.java

--- FullPersistence.java
+++ FullPersistence.java
@@ -1,5 +1,11 @@
 package addressbook;
 
+/*
+MVC-Teil: Modell
+Damit die Möglichkeit bleibt, später zu anderen Formen der Persistenz zu wechseln,
+ist diesen Interfacetyp erstellt.
+*/
+
 public interface FullPersistence {
     AddressBookDataModel loadBook() throws AddressBookException;
     
