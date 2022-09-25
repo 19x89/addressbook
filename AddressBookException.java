9bd988d3523b748e9be3c280aa5c1e0000e3cb05
19x89 <33625931+19x89@users.noreply.github.com>
2022-09-25 12:47:49 +0200

    Update AddressBookException.java

--- AddressBookException.java
+++ AddressBookException.java
@@ -1,5 +1,12 @@
 package addressbook;
 
+/*
+MVC-Teil: Modell
+Für das Weiterreichen von Ausnahmen (mit Hilfe von Exception-Chainings),
+die beim Laden oder Speichern von Adressbüchern auftreten können,
+ist diese Exception-Klasse erstellt.
+*/
+
 public class AddressBookException extends Exception {
     public AddressBookException( String message ) {
         super( message );
