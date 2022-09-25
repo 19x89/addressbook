ea6bd396b0c70b8711a6d459adbebe173ce79885
19x89 <33625931+19x89@users.noreply.github.com>
2022-09-25 12:26:05 +0200

    Update AddressBookDataModel.java

--- AddressBookDataModel.java
+++ AddressBookDataModel.java
@@ -2,6 +2,12 @@ package addressbook;
 
 import java.util.Iterator;
 
+/*
+MVC-Teil: Modell
+Diese Schnittstelle erleichtert zukünftigen Äderungen, wenn das Datenmodell geändert wird
+(z.B. von ArrayList zur Datenbank)
+*/
+
 public interface AddressBookDataModel {
     void addPerson( Person p );
     
